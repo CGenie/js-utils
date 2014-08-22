649a4ad59a1a86d32590e278838e77f6b292935d
Przemek Kaminski <cgenie@gmail.com>
2014-08-22 11:06:08 +0200

    DATE: add with_weeks function

--- src/date.js
+++ src/date.js
@@ -3,6 +3,38 @@
 define([
 ], function(
 ) {
+    'use strict';
+
+    var with_weeks = function(period, add_zeros) {
+        var keys = ['years', 'months', 'weeks', 'days'],
+            weeks,
+            days,
+            period_c;
+
+        if(period === undefined) {
+            return;
+        }
+
+        period_c = JSON.parse(JSON.stringify(period));
+
+        // 'weeks' is not sent in period: recompute it
+        weeks = period_c.weeks || 0;
+        days = period_c.days || 0;
+
+        if(days % 7 == 0) {
+            period_c.weeks = weeks + parseInt(days/7);
+            period_c.days = 0;
+        }
+
+        if(!!add_zeros) {
+            for(var i = 0; i < keys.length; i++) {
+                period_c[keys[i]] = period_c[keys[i]] || 0;
+            }
+        }
+
+        return period_c;
+    };
+
     // Formats dict of form
     // {'months': x, 'days': x}
     // into a string 'x months, x days'.
@@ -10,25 +42,18 @@ define([
     // Optionally can omit x = 1 and write just 'month'
 
     var format_period = function(period, omit_ones) {
+        var keys = ['year', 'month', 'week', 'day'],
+            ret = [],
+            period_c = with_weeks(period);
+
         if(period === undefined) {
             return '';
         }
 
-        var keys = ['year', 'month', 'week', 'day'];
-        var ret = [];
-
-        // 'weeks' is not sent in period: recompute it
-        var weeks = period.weeks || 0;
-        var days = period.days || 0;
-        if(days % 7 == 0) {
-            period.weeks = weeks + parseInt(days/7);
-            period.days = 0;
-        };
-
         for(var i = 0; i < keys.length; i++) {
             var key = keys[i];
             var key_plural = key + 's';
-            var val = period[key_plural];
+            var val = period_c[key_plural];
             var str = '';
 
             if(!!val) {
@@ -44,12 +69,13 @@ define([
 
                 ret.push(str);
             }
-        };
+        }
 
         return ret.join(', ');
-    }
+    };
 
     return {
-        format_period: format_period
-    }
+        format_period: format_period,
+        with_weeks: with_weeks
+    };
 });
